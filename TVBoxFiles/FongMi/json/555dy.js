db36e9cebb337c1e386ab926447f016954d804ad
Garky <100259948+GarkyGod@users.noreply.github.com>
2023-10-31 01:24:49 +0800

    Update 555dy.js

--- TVBoxFiles/FongMi/json/555dy.js
+++ TVBoxFiles/FongMi/json/555dy.js
@@ -1,5 +1,6 @@
+
 var rule = {
-    title: '555电影[V2]', // csp_AppYsV2
+    title: '555电影[V2]', // csp_AppYsV2 // https://www.555dianying.cc/
 	host:'https://www.555dyy.top',
 	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,".row:eq(2)&&a&&href");print(src);HOST=src+"/api.php/app/"',
     url: 'video?tid=fyclassfyfilter&limit=20&pg=fypage',
